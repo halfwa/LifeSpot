44f59c040d29f04c2e1a49231d0f3ba9528bf1da
Yaroslav Shamov <yaroslav.shamov2@mail.ru>
2023-10-18 01:57:47 +0300

    Добавляет функционал комментариев на странице

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,1 +1,27 @@
-﻿alert('Здесь можно получить информацию о проекте.');
+﻿//alert('Здесь можно получить информацию о проекте.');
+const writeComment = review => {
+    document.getElementsByClassName('reviews')[0].innerHTML += `<div class="review-text">\n` +
+        `<p><i><b>${review['userName']}</b>  ${review['date']}</i></p>\n` +
+        `<p>${review['comment']}</p>\n` +
+        `</div >`;
+}
+
+function getReview() {
+
+    let review = {};
+
+    review['userName'] = prompt('Введите ваше имя');
+    if (review['userName'] == null) {
+        return
+    }
+
+    review['comment'] = prompt('Оставьте свой комментарий');
+    if (review['comment'] == null) {
+        return
+    }
+
+    review['date'] = new Date().toLocaleTimeString();
+
+    return writeComment(review);
+}
+
