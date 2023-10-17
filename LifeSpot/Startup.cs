dd8eb601d9f752d920805c8ccaf8a54de187b5a6
Yaroslav Shamov <yaroslav.shamov2@mail.ru>
2023-10-17 19:32:45 +0300

    Добавляет страницу about в проект

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -57,6 +57,18 @@ namespace LifeSpot
                     await context.Response.WriteAsync(html.ToString());
                 });
 
+                endpoints.MapGet("/about", async context => 
+                {
+                    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "about.html");
+
+                    // Загружаем шаблон страницы, вставляя в него элементы
+                    var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                        .Replace("<!--SIDEBAR-->", sideBarHtml)
+                        .Replace("<!--FOOTER-->", footerHtml);
+
+                    await context.Response.WriteAsync(html.ToString());
+                });
+
                 endpoints.MapGet("/Static/CSS/index.css", async context =>
                 {
                     var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css");
@@ -74,6 +86,13 @@ namespace LifeSpot
                 endpoints.MapGet("/Static/JS/testing.js", async context =>
                 {
                     var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "testing.js");
+                    var js = await File.ReadAllTextAsync(jsPath);
+                    await context.Response.WriteAsync(js);
+                });
+
+                endpoints.MapGet("/Static/JS/about.js", async context =>
+                {
+                    var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "about.js");
                     var js = await File.ReadAllTextAsync(jsPath);
                     await context.Response.WriteAsync(js);
                 });
--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,28 +1,40 @@
-﻿let sessionHandler = function() {
+﻿
+/*
+* Сессия теперь создается в общей области видимости.
+* Будет "захватываться" тремя функциями
+* 
+* */
+let session = new Map();
 
-    let session = new Map();
+function handleSession() {
 
-    session.set("userAgent", window.navigator.userAgent)
+    session.set("userAgent", window.navigator.userAgent);
+    session.set("startDate", new Date().toLocaleString());
+
+    return session;
+}
+
+function checkAge() {
+
     session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
 
     if (session.get("age") >= 18) {
-
-        session.set("startDate", new Date().toLocaleString());
-
-        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + session.get('startDate'));
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
     }
     else {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-        window.location.href = "http://www.google.com";
+        window.location.href = "http://www.google.com"
     }
+}
 
-    // Вывод в консоль
+let sessionLog = function logSession() {
+
     for (let result of session) {
         console.log(result)
     }
 }
 
-function filterContent(inputString) {
+function filterContent() {
 
     let elements = document.getElementsByClassName('video-container');
 
@@ -31,11 +43,16 @@ function filterContent(inputString) {
         let videoContainer = elements[i];
         let videoTitle = videoContainer.getElementsByTagName('h3')[0].textContent.toLowerCase();
 
-        if (videoTitle.includes(inputString)) {
+        if (videoTitle.includes(inputParseFunction())) {
             videoContainer.setAttribute('style', 'display: inline-block');
         }
         else {
             videoContainer.setAttribute('style', 'display: none');
         }
     }
+}
+
+function offerSubscribe() {
+
+    setTimeout(() => alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @halfwa!"), 60000);
 }
