384d4c93a04cac1fa76422e63fdb24c03d223abe
Yaroslav Shamov <yaroslav.shamov2@mail.ru>
2023-10-22 19:01:20 +0300

    Добавляет счетчик лайков для комментариев

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,28 +1,33 @@
-﻿/*
-* Запросим пользовательский ввод
-* и сохраним отзыв в объект
-* 
-* */
-function getComment() {
-    // Создадим объект
-    let comment = {}
-    
-    // Сохраним свойство имени
-    comment.userName = prompt("Как вас зовут ?")
-    if (comment.userName == null){
+﻿
+function Comment() {
+    // Запросим имя
+    this.author = prompt("Как вас зовут ?")
+    if (this.author == null) {
+        this.empty = true
         return
     }
-    
-    // Сохраним текст отзыва
-    comment.text = prompt("Напишите свой отзыв")
-    if (comment.text == null){
-        return
-    }
-    
-    comment.date = new Date().toLocaleString()
 
-    const result = confirm('Хотите что бы другие пользователи могли оценить ваш комментарий?');
-    if (result) {
+    // Запросим текст
+    this.text = prompt("Оставьте отзыв")
+    if (this.text == null) {
+        this.empty = true
+        return
+    }
+
+    // Сохраним текущее время
+    this.date = new Date().toLocaleString()
+}
+
+function addComment() {
+    // Создадим объект
+    let comment = new Comment();
+
+    if (comment.empty) {
+        return;
+    }
+    const enableLikes = confirm('Хотите что бы другие пользователи могли оценить ваш комментарий?');
+
+    if (enableLikes) {
 
         let review = Object.create(comment);
         review.rate = 0;
@@ -41,7 +46,8 @@ const writeComment = review => {
 
     let likeCounter = '';
     if (review.hasOwnProperty('rate')) {
-        likeCounter = '<b>Рейтинг: </b>' + review.rate;
+        let commentId = Math.random();
+        likeCounter = `<b>Рейтинг: </b> <button id ="${commentId}" onclick=addLike(this.id)>❤️ ${review.rate}</button>`;
     }
 
     document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
@@ -50,4 +56,23 @@ const writeComment = review => {
         '</div>';
 }
 
-        
+function addLike(id) {
+    // Найдём нужный элемент по id
+    let element = document.getElementById(id);
+
+    // Преобразуем текст элемента в массив, разбив его по пробелам (так как счётчик лайков у нас отделен от символа ❤️пробелом)
+    let array = element.innerText.split(' ');
+
+    // Вытащим искомое значение счётчика и сразу же преобразуем его в число, так как
+    // при сложении любого значения со строкой в JS будет строка, а нам этого не требуется
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    // Увеличим счётчик
+    resultNum++;
+
+    // Сохраним измененное значение обратно в массив
+    array[array.length - 1] = `${resultNum}`;
+
+    // Обновим текст элемента
+    element.innerText = array.join(' ');
+}        
--- LifeSpot/Static/JS/testing.js
+++ LifeSpot/Static/JS/testing.js
@@ -1,4 +1,21 @@
-﻿// Создаем объект Map
+﻿/*
+* Обработчик клика
+*
+* */
+const saveInput = function () {
+    // Вытащим значение текстового поля (как у нас уже делается при фильтрации)
+    let currentInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+
+    // Покажем окно с прошлым и новым значением
+    alert('Последний ввод: ' + this.lastInput /* равноценно window.lastInput, так как мы работаем в контексте браузера */
+        + '\n' + 'Текущий ввод: ' + currentInput);
+
+    // Сохраним новое значение в контекст
+    this.lastInput = currentInput;
+}
+
+
+// Создаем объект Map
 let myMap = new Map();
 myMap.set("Германия", "Берлин")
 myMap.set("Швеция", "Стокгольм")
@@ -16,3 +33,4 @@ let newArrayOfStrings = [];
 // Позволяет на лету выполнять операции с парой ключ-значение
 Array.from(myMap, ([key,value]) => newArrayOfStrings.push(`${key} - ${value}`) );
 console.log(newArrayOfStrings);
+
